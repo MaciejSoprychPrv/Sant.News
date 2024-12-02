dee1180a7124017c5acc4a6a6e851205140d797c
Maciej Soprych <maciej.soprych@tpa-group.pl>
2024-12-02 16:21:50 +0100

    Logs for specific number of stories

--- Sant.News/HackerNews/GetHackerNews.cs
+++ Sant.News/HackerNews/GetHackerNews.cs
@@ -121,11 +121,15 @@ namespace Sant.News.HackerNews
                 var result = _mapper.Map<List<GetHackerNewsDto>>(rawStoriesDetails);
                 _logger.LogInformation("Mapping to DTO completed");
 
+                
                 result = result.OrderByDescending(c => c.Score).ToList();
+                _logger.LogInformation("Ordered descending");
 
                 var storiesCount = CalculateStorieCount(request.StoriesCount, result);
+                _logger.LogInformation("Number of stories to be taken");
 
                 result = result.Take(storiesCount).ToList();
+                _logger.LogInformation("Ranged to the number of requested stories");
 
                 return Result.Ok(result);
             }
