598588282c79ed532181d422882f9c64b6121800
Maciej Soprych <maciej.soprych@tpa-group.pl>
2024-12-02 14:33:20 +0100

    Injection of connection options and cache to IdsProcessing

---
+++ Sant.News/HackerNews/IdsProcessing.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace Sant.News.HackerNews
+{
+    public class IdsProcessing
+    {
+        private readonly HackerNewsConnectionOptions _hackerNewsConnectionOptions;
+        private readonly IMemoryCache _cache;
+
+        public IdsProcessing(IOptions<HackerNewsConnectionOptions> hackerNewsConnectionOptions, IMemoryCache cache)
+        {
+            _hackerNewsConnectionOptions = hackerNewsConnectionOptions.Value;
+            _cache = cache;
+        }
+    }
+}
