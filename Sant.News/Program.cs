df9b8b6c72dac66b12c771d201109c82e0d2c03c
Maciej Soprych <maciej.soprych@tpa-group.pl>
2024-12-02 13:55:36 +0100

    Basic registration of component and services

--- Sant.News/Program.cs
+++ Sant.News/Program.cs
@@ -1,3 +1,5 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHangfire(c => c
+    .UseInMemoryStorage()
+    .UseFilter(new AutomaticRetryAttribute { Attempts = 2 }));
+
+var options = new BackgroundJobServerOptions
+{
+    Queues = new[] { "hackernews" },
+    WorkerCount = 10,
+};
+
+builder.Services.AddHangfireServer(serverOptions =>
+{
+    serverOptions.Queues = options.Queues;
+    serverOptions.WorkerCount = options.WorkerCount;
+});
+
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
