a52544717a8d99c1263d522a45e1c5e8d30dace0
Maciej Soprych <maciej.soprych@tpa-group.pl>
2024-12-02 14:04:05 +0100

    Mediator incjected

---
+++ Sant.News/HackerNews/GetHackerNews.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sant.News.HackerNews
+{
+    [ApiController]
+    public class GetHackerNews : ControllerBase
+    {
+        public const string Url = "/api/hackerNews/{storiesCount}";
+        private readonly IMediator _mediator;
+
+        public GetHackerNews(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+       
+    }
+}
